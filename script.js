c2a8ced369c18f3cef3c4debc5b277aa440e3895
Erfan Bahramnia <erfanbahramnia@gmail.com>
2022-04-20 14:30:42 +0430

    information taked from api and stored in localstorage

---
+++ script.js
@@ -0,0 +1,15 @@
+function callApi(city) {
+    const cityName = city;
+    const apiKey = "0c785bb1b3c3a84ff730f463a9c8c710"
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`)
+        .then(response => response.json())
+        .then(json => {
+            // get important information from api
+            const {weather, main, wind, clouds, name} = json;
+
+            const data = {weather, main, wind, clouds, name};
+            localStorage.setItem(city, JSON.stringify(data))
+            console.log(data)
+        });
+}
+
